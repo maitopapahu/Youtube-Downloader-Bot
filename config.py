66f6599cd9c00e6b0c49859a63bfba5300341bfe
maitopapahu <owaisbabakatv@gmail.com>
2025-03-28 10:31:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("7457376044:AAGsj_PHrK8GqMUKIIUIq4bC1y6wBbX_SO4")
+BOT_TOKEN = os.environ.get("7912629645:AAEaJW9JblAEgy5DGaMpHNlHq0X2ljBZG0k")
 APP_ID = int(os.environ.get("29696002"))
 API_HASH = os.environ.get("b0402db44bb51470d0c50d3be950781b")
 
